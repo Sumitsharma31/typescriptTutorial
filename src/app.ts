925b2e240c3c284a02cb85a8fc39a5420cf8f930
sumit sharma <ssumit20kr@gmail.com>
2026-01-13 22:40:00 +0530

    typescript annotation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import console = require("node:console");
+
 function greet(name:string):string{
   return `Hello ${name} Welcome Back`
 }
@@ -6,4 +8,13 @@ console.log(greet('Sumit'));
 // infrensing in ts
 
 let food="bread";
-food=5;
+// food=5;
+
+// Annotation in ts
+let games:string='football';
+console.log(games);
+
+let userDetail:string | number;
+
+userDetail='Bihar'
+userDetail=7788995544;
